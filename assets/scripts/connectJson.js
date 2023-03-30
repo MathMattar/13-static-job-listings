e1e760f53b75de308979e12650c0c76670b209aa
MathMattar <mathmattar@hotmail.com>
2023-03-30 16:06:59 -0300

    Connect json

--- assets/scripts/connectJson.js
+++ assets/scripts/connectJson.js
@@ -1,6 +1,6 @@
 export default async function getData() {
   const response = await fetch("./assets/json/data.json");
   const data = await response.json();
-  console.log(data);
+
   return data;
 }
